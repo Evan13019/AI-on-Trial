0cef0f2b77bfe98b41dcd173ab71d07c0b6e7da8
Evan13019 <137565148+Evan13019@users.noreply.github.com>
2025-07-08 18:55:57 -0400

    added .env testing to demo file

--- demo.py
+++ demo.py
@@ -1,2 +1,10 @@
-print("demo works!")
+# print("demo works!")
 # just use this to play around 
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv("ANTHROPIC_API_KEY")
+
+print(api_key)
